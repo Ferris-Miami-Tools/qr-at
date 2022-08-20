77726b92a0a23531fe94d5c33da3d4add6c10f25
Joshua Ferris <jajjferris@gmail.com>
2022-08-20 19:46:50 -0400

    :bug: firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,27 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY || "AIzaSyDDc8TNSp6Nyo9KOUliE7Myjzq2CNE_3x8",
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN || "qr-at-prod.firebaseapp.com",
-  projectId: import.meta.env.VITE_PROJECT_ID || "qr-at-prod",
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET || "qr-at-prod.appspot.com",
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID || "110682686293",
-  appId: import.meta.env.VITE_APP_ID || "1:110682686293:web:abbb66e1c65b86981165dc",
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID || "G-84ECD6BJSN"
+let firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
+
+if (import.meta.env.MODE == "production") {
+  firebaseConfig = {
+    apiKey: "AIzaSyDDc8TNSp6Nyo9KOUliE7Myjzq2CNE_3x8",
+    authDomain: "qr-at-prod.firebaseapp.com",
+    projectId: "qr-at-prod",
+    storageBucket: "qr-at-prod.appspot.com",
+    messagingSenderId: "110682686293",
+    appId: "1:110682686293:web:abbb66e1c65b86981165dc",
+    measurementId: "G-84ECD6BJSN",
+  };
+}
 
 const app = initializeApp(firebaseConfig);
 
