1c3b3f60577998a6e89997bc4f07406913c804de
Joshua Ferris <jajjferris@gmail.com>
2022-09-17 23:55:45 -0400

    :sparkles: class timer

--- src/components/ClassSearch.vue
+++ src/components/ClassSearch.vue
@@ -1,12 +1,66 @@
 <script setup>
-  import { ref, onBeforeUnmount, computed } from "vue";
+  import { ref, onBeforeUnmount, computed, reactive } from "vue";
   import { query, collection, where, getDocs, onSnapshot, updateDoc, doc, addDoc, serverTimestamp } from "firebase/firestore";
   import { db } from "../firebase";
   import store from "../store";
   import Toggle from "./Toggle.vue";
-  onBeforeUnmount(() => clearSearch());
-  const searching = ref(false);
+
+  const updateDateState = () => {
+    console.log("Date state interval fired");
+    dateState.current = new Date();
+    
+    // If no class early return
+    if (dateState.class.startHour == null || dateState.class.startMinute == null || dateState.class.endHour == null || dateState.class.endMinute == null) return;
+
+    // Update start and end date objects
+    dateState.class.start = new Date(`${searchDate.value}T${leadingZero(dateState.class.startHour)}:${leadingZero(dateState.class.startMinute)}:00`);
+    dateState.class.end = new Date(`${searchDate.value}T${leadingZero(dateState.class.endHour)}:${leadingZero(dateState.class.endMinute)}:00`);
+
+    if (dateState.current >= dateState.class.start && dateState.current <= dateState.class.end) {
+      // Class in progress
+      console.log("Class in progress");
+      const curTSM = (dateState.current.getHours() * 60) + dateState.current.getMinutes();
+      dateState.classProgress = ((curTSM - dateState.class.startTSM)/(dateState.class.endTSM - dateState.class.startTSM)) * 100;
+    } else if (dateState.current < dateState.class.start) {
+      // Class has not started yet
+      console.log("Class has not started yet");
+      dateState.classProgress = 0;
+    } else {
+      // Class is in the past
+      console.log("Class is in the past");
+      dateState.classProgress = 100;
+      clearInterval(dateState.interval);
+    }
+  };
+
   const searchDate = ref(null);
+  const dateState = reactive({
+    current: new Date(),
+    class: {
+      start: null,
+      startTSM: null,
+      startHour: null,
+      startMinute: null,
+      startString: null,
+      end: null,
+      endTSM: null,
+      endHour: null,
+      endMinute: null,
+      endString: null,
+    },
+    interval: setInterval(updateDateState, 1000 * 10), // 10 second interval
+    classProgress: 0,
+  });
+  const leadingZero = (num) => {
+    return String(num).padStart(2, '0');
+  };
+
+  onBeforeUnmount(() => {
+    clearSearch()
+    clearInterval(dateState.interval);
+  });
+  const searching = ref(false);
+  
   const searchSections = ref([]);
   const searchSub = ref(null);
   const students = ref(null);
@@ -20,6 +74,7 @@
     const onlyExcused = students.value.filter(el => el.excused);
     return onlyExcused.length;
   });
+  
   const allSectionHaveClass = async (sections, date) => {
     let allGood = true;
     for (const section of sections) {
@@ -33,10 +88,26 @@
       if (qsnap.empty) {
         store.actions.errorToast(`${section} did not have class on ${date}.`);
         allGood = false;
+      } else {
+        const classData = qsnap.docs[0].data();
+        
+        // Set date state
+        dateState.class.startTSM = classData.start + 15;
+        dateState.class.endTSM = classData.end - 15;
+        dateState.class.startHour = Math.floor(dateState.class.startTSM / 60);
+        dateState.class.startMinute = dateState.class.startTSM - (dateState.class.startHour * 60);
+        dateState.class.startString = `${dateState.class.startHour % 12}:${leadingZero(dateState.class.startMinute)}${dateState.class.startHour >= 12 ? 'PM' : 'AM'}`;
+        dateState.class.endHour = Math.floor(dateState.class.endTSM / 60);
+        dateState.class.endMinute = dateState.class.endTSM - (dateState.class.endHour * 60);
+        dateState.class.endString = `${dateState.class.endHour % 12}:${leadingZero(dateState.class.endMinute)}${dateState.class.endHour >= 12 ? 'PM' : 'AM'}`;
+
+        // Force immediate update of date state
+        updateDateState();
       }
     }
     return allGood;
   };
+  
   const studentsInSections = async sections => {
     let studs = [];
     const q = query(
@@ -55,6 +126,16 @@
     if (searchSub.value) searchSub.value();
     searchSub.value = null;
     students.value = null;
+
+    // Reset date state
+    dateState.class.startTSM = null;
+    dateState.class.endTSM = null;
+    dateState.class.startHour = null;
+    dateState.class.startMinute = null;
+    dateState.class.startString = null;
+    dateState.class.endHour = null;
+    dateState.class.endMinute = null;
+    dateState.class.endString = null;
   };
   const searchClasses = async () => {
     console.log("Search Classes");
@@ -169,6 +250,28 @@
         <div v-if="students">
           <h3 class="font-bold text-center">{{ searchDate }} - {{ searchSections.join(", ") }}</h3>
           <p class="text-xs text-center">Students: {{ students.length }} (P={{ presentCount }}, E={{ excusedCount }}, A={{ students.length - presentCount - excusedCount }})</p>
+
+          <!-- In class progress bar -->
+          <div
+            v-if="dateState.current >= dateState.class.start"
+            class="w-full mt-4"
+          >
+            <div class="w-full bg-gray-200 rounded">
+              <div :class="`${dateState.classProgress == 100 ? 'bg-green-500' : dateState.classProgress >= 90 ? 'bg-red-500' : 'bg-yellow-500'} text-xs font-medium text-green-100 text-center p-0.5 leading-none rounded`" :style="`width: ${dateState.classProgress}%`">
+                {{Math.round(dateState.classProgress)}}%
+              </div>
+            </div>
+            <div class="flex justify-between">
+              <p>{{ dateState.class.startString }}</p>
+              <p>{{ dateState.class.endString }}</p>
+            </div>
+            <p v-if="dateState.current <= dateState.class.end" class="text-xs text-center">{{ Math.round((dateState.class.endTSM - dateState.class.startTSM) * (1 - (dateState.classProgress / 100))) }} minutes remaining</p>
+          </div>
+          <!-- Before class Time until class -->
+          <p v-if="(new Date(`${searchDate}T12:00:00`)).getDate() == dateState.current.getDate() && dateState.current < dateState.class.start">
+            Time until class: {{ Math.round((((dateState.class.start - dateState.current) % 86400000)% 3600000)/ 60000) }} minutes
+          </p>
+          <hr class="mt-4" />
           <button @click="clearSearch" class="mt-4 w-full bg-transparent hover:bg-purple-400 text-purple-700 font-semibold hover:text-white py-3 px-4 border border-purple-400 hover:border-transparent rounded">
             New Search
           </button>
